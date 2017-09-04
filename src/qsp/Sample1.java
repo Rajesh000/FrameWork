c34b306b5a4d670f64aec7a511e1d92ed115c8db
admin <admin@admin-PC>
2017-09-04 18:50:36 +0530

    Second Commit

---
+++ src/qsp/Sample1.java
@@ -0,0 +1,14 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample1 
+{
+	 @Test
+	 public void Demo2()
+	 {
+	 	Reporter.log("HELLO Jspiders - Hebbal",true);
+	 }
+
+}
