018748a78f808aba3f53a49f42086f89924684dc
admin <admin@admin-PC>
2017-09-04 18:29:38 +0530

    First Commit

---
+++ src/qsp/Sample.java
@@ -0,0 +1,14 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample
+{
+ @Test
+public void Demo()
+{
+	Reporter.log("HELLO Qspiders",true);
+}
+
+}
